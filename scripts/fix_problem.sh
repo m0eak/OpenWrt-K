0497ebac48c9eb0fd895522b1edb479599b96dc5
m0eak <zhongliangpsp@sina.com>
2024-06-01 17:51:45 +0800

    删除自带的tailscale

--- scripts/fix_problem.sh
+++ scripts/fix_problem.sh
@@ -6,7 +6,9 @@ sed -i 's/^  DEPENDS:= +kmod-crypto-manager +kmod-crypto-pcbc +kmod-crypto-fcryp
 sed -i 's/^	dnsmasq \\$/	dnsmasq-full \\/g' ./include/target.mk
 sed -i 's/^	b43-fwsquash.py "$(CONFIG_B43_FW_SQUASH_PHYTYPES)" "$(CONFIG_B43_FW_SQUASH_COREREVS)"/	$(TOPDIR)\/tools\/b43-tools\/files\/b43-fwsquash.py "$(CONFIG_B43_FW_SQUASH_PHYTYPES)" "$(CONFIG_B43_FW_SQUASH_COREREVS)"/' ./package/kernel/mac80211/broadcom.mk
 # Tailscale
-[ -e "$OPENWRT_ROOT_PATH/feeds/packages/net/tailscale/Makefile" ] && sed -i '/\/etc\/init\.d\/tailscale/d;/\/etc\/config\/tailscale/d;' feeds/packages/net/tailscale/Makefile && echo "Tailscale修复完成"
+[ -d "$OPENWRT_ROOT_PATH/feeds/packages/net/tailscale" ] && echo "找到feeds内的Tailscale" && find $OPENWRT_ROOT_PATH/feeds/packages/net/ | grep Makefile | grep tailscale | xargs rm -f && echo "已删除Feeds内的Tailscale"
+[ -d "$OPENWRT_ROOT_PATH/package/feeds/packages/tailscale" ] && echo "找到feeds/packages内的Tailscale" && find $OPENWRT_ROOT_PATH/package/feeds/packages | grep Makefile | grep tailscale | xargs rm -f && echo "已删除feeds/packages内的Tailscale"
+# [ -e "$OPENWRT_ROOT_PATH/feeds/packages/net/tailscale/Makefile" ] && sed -i '/\/etc\/init\.d\/tailscale/d;/\/etc\/config\/tailscale/d;' feeds/packages/net/tailscale/Makefile && echo "Tailscale修复完成"
 # 固定Vermagic
 if grep -q "x86_64" $GITHUB_WORKSPACE/config/"$config"/target.config; then
   echo "固定Vermagic"
